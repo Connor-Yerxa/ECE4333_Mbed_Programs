9fc4917e6853a6055142cf00e4cd48fbd88a6639
connor-yerxa <cyerxa@unb.ca>
2026-04-08 15:13:41 -0300

    Start work on line_following.
    wall following still agressive. also fixed integrator.

--- Line_Following/main.cpp
+++ Line_Following/main.cpp
@@ -8,6 +8,18 @@ using namespace std::chrono;
 
 Mutex mPg;
 
+void clampfloat(float * x, float max)
+{
+    if(*x > max) *x = max;
+    if(*x < -1*max) *x = -1 * max;
+}
+
+void clampint(int * x, int max)
+{
+    if(*x > max) *x = max;
+    if(*x < -1*max) *x = -1 * max;
+}
+
 // Function prototypes
 void PiControlThread(void const *argument);
 void PeriodicInterruptISR(void);
@@ -16,6 +28,16 @@ void ExtInterruptISR(void);
 void WatchdogThread(void const *argument);
 void WatchdogISR(void const *n);
 
+void update_Speeds_to_line(
+    float * leftVel, 
+    float * rightVel, 
+    uint16_t us_front, 
+    uint16_t us_right, 
+    uint16_t us_front_right, 
+    uint16_t us_right_front,
+    float ideal_distance
+);
+float us2mm(uint16_t us);
 void applyPI(PwmOut * pwm, float * currentVel, float idealVel, int16_t dP, int16_t dT, float * integration, DigitalOut * MDIR);
 void read_serial(UnbufferedSerial * serial);
 
@@ -54,10 +76,14 @@ int duty0=0, duty1=0;
 Ticker PeriodicInt;
 
 //PI controller vars
-float kp = 10, ki = 2;
+float kp = 10, ki = 4, distance_kp=0.008;
 float currentVel0=0, currentVel1=0;
-float idealVel0 = 0;
-float idealVel1 = 0;
+float offsetVel = 1.5, maxVel=3;
+float idealVel0 = 0; // Left
+float idealVel1 = 0; // Right
+float ideal_distance = 300;
+float sensor_rig_radius = 90; // mm
+bool run_Wall_Following = false;
 // float e0;
 int stepsPerRotation = 1216;
 int16_t dP0=0, dT0=1, dP1=0, dT1=1;
@@ -66,6 +92,7 @@ int16_t dP0=0, dT0=1, dP1=0, dT1=1;
 UnbufferedSerial pc(USBTX, USBRX);
 SPI FPGA(PB_5, PB_4, PB_3);
 
+uint16_t USR0, USR1, USR2, USR3;
 
 void ResetFPGA()
 {
@@ -95,11 +122,12 @@ void init()
     FPGA.frequency(500000);
 
     bt.baud(9600);
+    pc.baud(115200);
 
     ResetFPGA();
     ResetFPGA_SPI();
     
-    ID = FPGA.write(0x8004); // ID of SPI slave is returned as 0x0017
+    ID = FPGA.write(0x8008); // ID of SPI slave is returned as 0x0017
 
     pwm0.period_us(period); // This sets the PWM period to 500 us.
 
@@ -118,6 +146,11 @@ void init()
     dT0 = FPGA.write(Dummy); // Read QEI-0 time interval
     dP1 = FPGA.write(Dummy); // Read QEI-1 position register 
     dT1 = FPGA.write(Dummy); // Read QEI-1 time interval
+
+    USR0 = FPGA.write(Dummy);
+    USR1 = FPGA.write(Dummy);
+    USR2 = FPGA.write(Dummy);
+    USR3 = FPGA.write(Dummy);
 }
 
 // main() runs in its own thread in the OS
@@ -129,6 +162,7 @@ int main()
 
     while (true) {
         float cvel, idvel, err, cvel1, idvel1;
+        uint16_t u0, u1, u2, u3;
         Kernel::Clock::time_point now = Kernel::Clock::now();
         if(now - lastPrint > printInterval)
         {
@@ -136,14 +170,21 @@ int main()
             // bt.write(&c, 1);
             lastPrint = now;
             mPg.lock();
-            cvel = currentVel0;
+            // cvel = currentVel0;
             idvel = idealVel0;
-            cvel1 = currentVel1;
+            // cvel1 = currentVel1;
             idvel1 = idealVel1;
+            u0 = USR0;
+            u1 = USR1;
+            u2 = USR2;
+            u3 = USR3;
             mPg.unlock();
-            printf("\nC0: %d I0: %d\n", (int)(cvel * 100), (int)(idvel * 100));
-            printf("C1: %d I1: %d\n", (int)(cvel1 * 100), (int)(idvel1 * 100));
-            printf("%d %d\n", (int)(ki*10), (int)(kp*10));
+            // printf("\nC0: %d I0: %d\n", (int)(cvel * 100), (int)(idvel * 100));
+            // printf("C1: %d I1: %d\n", (int)(cvel1 * 100), (int)(idvel1 * 100));
+            // printf("%d %d\n", (int)(ki*10), (int)(kp*10));
+
+            // printf("%d %d %d %d\n", u0, u1, u2, u3);
+            printf("%d %d\n", (int)(idvel * 100), (int)(idvel1 * 100));
         }
 
         read_serial(&bt);
@@ -168,7 +209,17 @@ void PiControlThread(void const *argument) {
         dP1 = FPGA.write(Dummy); // Read QEI-1 position register 
         dT1 = FPGA.write(Dummy); // Read QEI-1 time interval
 
+        USR0 = FPGA.write(Dummy);
+        USR1 = FPGA.write(Dummy);
+        USR2 = FPGA.write(Dummy);
+        USR3 = FPGA.write(Dummy);
+
         // printf("%d %d %d %d\n", dP0, dT0, dP1, dT1);
+
+        if(run_Wall_Following)
+        {
+            update_Speeds_to_line(&idealVel0, &idealVel1, USR0, USR3, USR1, USR2, ideal_distance);
+        }
         
         applyPI(&pwm0, &currentVel0, idealVel0, dP0, dT0, &integration0, &MDIR0);
         applyPI(&pwm1, &currentVel1, idealVel1, dP1, dT1, &integration1, &MDIR1);
@@ -191,7 +242,7 @@ void applyPI(PwmOut * pwm, float * currentVel, float idealVel, int16_t dP, int16
         float e = idealVel - *currentVel;
         // printf("%d\n", (int)(e*100));
 
-        *integration += ki*e;
+        *integration = (*integration + e);
         float propotion = kp * e;
 
         
@@ -199,10 +250,9 @@ void applyPI(PwmOut * pwm, float * currentVel, float idealVel, int16_t dP, int16
         if(*integration < -1*maxDuty) *integration = -1 * maxDuty;
 
 
-        duty = *integration + propotion;
+        duty = *integration*ki + propotion;
 
-        if(duty > maxDuty) duty = maxDuty;
-        if(duty < -1*maxDuty) duty = -1 * maxDuty;
+        clampint(&duty, maxDuty);
 
         bool dir = duty <= 0;
         *MDIR = dir;
@@ -227,45 +277,103 @@ void read_serial(UnbufferedSerial * serial)
         // serial->write(&key, 1);
 
         mPg.lock();
-        if(key == 'w')
+        if(!run_Wall_Following)
         {
-            idealVel0 += incremention;
-            idealVel1 += incremention;
-        } else if(key == 's')
+            if(key == 'w')
+            {
+                idealVel0 += incremention;
+                idealVel1 += incremention;
+            } else if(key == 's')
+            {
+                idealVel0 -= incremention;
+                idealVel1 -= incremention;
+            } else if(key == 'a')
+            {
+                idealVel0 -= incremention;
+                idealVel1 += incremention;
+            } else if(key == 'd')
+            {
+                idealVel0 += incremention;
+                idealVel1 -= incremention;
+            }
+        }
+
+        if(key == 'l')
         {
-            idealVel0 -= incremention;
-            idealVel1 -= incremention;
-        } else if(key == 'a')
-        {
-            idealVel0 -= incremention;
-            idealVel1 += incremention;
-        } else if(key == 'd')
-        {
-            idealVel0 += incremention;
-            idealVel1 -= incremention;
-        } else if(key == ' ')
+            run_Wall_Following = !run_Wall_Following;
+        }
+        
+        if(key == ' ')
         {
             idealVel0 = 0;
             idealVel1 = 0;
+            run_Wall_Following = false;
         }
-        if(key == 'i')
-        {
-            ki += 0.1;
-        }
-        if(key == 'I')
-        {
-            ki -= 0.1;
-        }
-        if(key == 'p')
-        {
-            kp += 0.1;
-        }
-        if(key == 'P')
-        {
-            kp -= 0.1;
-        }
+        // if(key == 'i')
+        // {
+        //     ki += 0.1;
+        // }
+        // if(key == 'I')
+        // {
+        //     ki -= 0.1;
+        // }
+        // if(key == 'p')
+        // {
+        //     kp += 0.1;
+        // }
+        // if(key == 'P')
+        // {
+        //     kp -= 0.1;
+        // }
         mPg.unlock();
         wait_us(100);
         key = '\n';
     }
+}
+
+void update_Speeds_to_line(
+    float * leftVel, 
+    float * rightVel, 
+    uint16_t us_front, 
+    uint16_t us_right, 
+    uint16_t us_front_right, 
+    uint16_t us_right_front,
+    float ideal_distance
+){
+    mPg.lock();
+    float mm_front = us2mm(us_front);
+    float mm_right = us2mm(us_right);
+    float mm_front_right = us2mm(us_front_right);
+    float mm_right_front = us2mm(us_right_front);
+    mPg.unlock();
+
+    float error;
+
+    error = ideal_distance - mm_right;
+    error += ideal_distance - (mm_right_front - sensor_rig_radius*(1/cos(3.14159/6) - 1));
+    // error += ideal_distance - (mm_right_front - sensor_rig_radius*(1/cos(3.14159/3) - 1));
+    error /= 2; // Average
+
+    // printf("%d\n", (int)(error*100));
+    // printf("%d %d\n", (int)(mm_right*100), (int)((mm_right_front - sensor_rig_radius*(1/cos(3.14159/6) - 1))*100));
+    // printf("%d\n", (int)((mm_right_front - sensor_rig_radius*(1/cos(3.14159/6) - 1))*100));
+    printf("%d\n", (int)(error*100));
+
+    mPg.lock();
+    *rightVel = distance_kp*error;
+    *leftVel = -1 * *rightVel;
+
+    if(mm_front > ideal_distance)
+    {
+        *leftVel += offsetVel;
+        *rightVel += offsetVel;
+    }
+    clampfloat(leftVel, maxVel);
+    clampfloat(rightVel, maxVel);
+    mPg.unlock();
+}
+
+float us2mm(uint16_t us)
+{
+    return (float)us / 5.83;
 }
